b9716dfd7071337b2bc1f5a3d4cda684fa153c19
jackarms <90922918+jackarms@users.noreply.github.com>
2022-03-20 21:54:11 -0500

    finished reaction routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const {
+  getAllThought,
+  getThoughtById,
+  addThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/").get(getAllThought);
+
+router.route("/:userId").post(addThought);
+
+router
+  .route("/:userId/:thoughtId")
+  .get(getThoughtById)
+  .put(updateThought)
+  .put(addReaction)
+  .delete(removeThought);
+
+router.route("/:userId/:thoughtId/:reactionId").delete(removeReaction);
+
+module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,10 +6,13 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
 } = require("../../controllers/user-controllers");
 
 router.route("/").get(getAllUser).post(createUser);
 
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route("/:id/friends/:friendId").put(addFriend);
 
 module.exports = router;
